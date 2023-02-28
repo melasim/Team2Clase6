1e0c22724410d82deda062df9e700a764da6c77f
melasim <melania.simes@hotmail.com>
2023-02-28 00:56:55 -0300

    primera version con componente main y lista

---
+++ src/components/Lista.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const lista = () => {
+    const [items, setItems] = useState([]);
+
+    function addItem() {
+        const newItem = {
+            id: items.length + 1,
+            text: `El articulo  ${items.length + 1} se ha añadido a la lista`
+        };
+        setItems([...items, newItem]);
+    
+    }
+    return (
+        <div>
+            <ul>
+                {items.map(item => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </ul>
+            <button onClick={addItem}>Agregar tu estado</button>
+        </div>
+    )
+}
+
+export default lista
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Lista from './Lista'
+const main = () => {
+    return (
+        <>
+            <h1>Lista de articulos!</h1>
+            <Lista/>
+        </>
+        
+
+    )
+}
+
+export default main
